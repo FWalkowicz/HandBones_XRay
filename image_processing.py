700e22b231c5e7ad0d1ea7cb0eac068eaa71d317
Filip Walkowicz <fillio00@wp.pl>
2023-08-29 10:07:20 +0200

    Image processing - structure

---
+++ image_processing.py
@@ -0,0 +1,45 @@
+"""
+
+"""
+
+
+class CreateDataset:
+    """
+    A class for creating and managing datasets of images.
+    This class provides methods to start the dataset creation process,
+    read images, and save images to the dataset.
+
+    """
+    def __int__(self):
+        """
+        Initializes an instance of the CreateDataset class.
+
+        :return: None
+        """
+        pass
+
+    def start(self):
+        """
+        Initiates the dataset creation process.
+
+        :return:
+        """
+        pass
+
+    def read_image(self):
+        """
+        Reads an image from a source.
+        This method is used to read an image from source and process it for the dataset.
+
+        :return:
+        """
+        pass
+
+    def save_image(self):
+        """
+        Saves an image to the dataset.
+        This method saves a previously read image to the dataset.
+
+        :return:
+        """
+        pass
