3aa98ba4837541baf0436d25e0778a78c50d841a
Filip Walkowicz <fillio00@wp.pl>
2023-08-30 14:03:27 +0200

    Structure - update project structure, add new file for training

--- app/image_processing.py
+++ app/image_processing.py
@@ -7,22 +7,16 @@ import supervision as sv
 from ultralytics import YOLO
 
 
-class CreateDataset:
-    """
-    A class for creating and managing datasets of images.
-    This class provides methods to start the dataset creation process,
-    read images, and save images to the dataset.
-
+class BoneSegmentation:
     """
 
-    def __int__(self, number_of_images):
+    """
+    def __int__(self):
         """
         Initializes an instance of the CreateDataset class.
 
-        :param number_of_images: number of images to convert
         :return: None
         """
-        self.number_of_images = number_of_images
         self.images_directory = os.path.join(
             os.getcwd(), "boneage-training-dataset/boneage-training-dataset"
         )
@@ -30,36 +24,6 @@ class CreateDataset:
         self.model = YOLO(
             "/home/filip/PycharmProjects/X-ray/runs/segment/yolo_custom/weights/best.pt"
         )
-
-    def start(self):
-        """
-        Initiates the dataset creation process.
-
-        :return:
-        """
-        pass
-
-    @staticmethod
-    def read_image(image_id):
-        """
-        Reads an image from a source.
-        This method is used to read an image from source and process it for the dataset.
-
-        :return:
-        """
-        image = cv2.imread(image_id)
-
-        return image
-
-    @staticmethod
-    def save_image(image, number):
-        """
-        Saves an image to the dataset.
-        This method saves a previously read image to the dataset.
-
-        :return: None
-        """
-        cv2.imwrite(f"prediction{number}", image)
 
     def predict(self, image):
         """
@@ -76,3 +40,7 @@ class CreateDataset:
         )
 
         return annotated_image
+
+    def multiple_prediction(self):
+        pass
+    
---
+++ app/train_model.py
@@ -0,0 +1,21 @@
+class CreateCustomModel:
+    """
+
+    """
+    def __int__(self, yaml_path: str, segmentation: bool = False, detection: bool = True):
+        self.yaml_path = ''
+        self.segmentation = segmentation
+        self.detection = detection
+
+    def download_dataset(self):
+        pass
+
+    def train(self):
+        pass
+
+    def validate(self):
+        pass
+
+    def test(self):
+        pass
+
