5a7d5d0212a31a23ae93533266eedac58fbe8cdc
Filip Walkowicz <fillio00@wp.pl>
2023-09-06 12:47:36 +0200

    FASTAPI - without metadata endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-from typing import Union
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.responses import StreamingResponse
 import shutil
 from pydantic import BaseModel
@@ -10,7 +9,8 @@ import os
 app = FastAPI()
 
 XRayStorage = {
-    "UniqueSessionId": str
+    "UniqueSessionId": None,
+    "Files": []
 }
 
 
@@ -25,26 +25,54 @@ class Item(BaseModel):
 
 
 @app.post("/executeAI")
-async def execute_ai(InputImage: UploadFile):
-    if not InputImage.filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+async def execute_ai(input_image: UploadFile):
+    """
+    Execute AI processing on an uploaded X-ray image.
+
+    :param input_image: The X-ray image file to be processed (JPEG or PNG).
+    :return: Information about the processed session
+    """
+    if not input_image.filename.lower().endswith(('.jpg', '.jpeg', '.png')):
         return {"error": "Only JPEG and PNG images are supported."}
 
     # Save the uploaded file to a temporary location
-    file_path = os.path.join("temp", InputImage.filename)
+    file_path = os.path.join("temp", input_image.filename)
     with open(file_path, "wb") as file:
-        file.write(InputImage.file.read())
+        file.write(input_image.file.read())
+    name = input_image.filename
     image = cv2.imread(file_path)
-    #os.remove(file_path)
+    os.remove(file_path)
 
-    processing = XRayPredictions(image=image)
+    processing = XRayPredictions(image=image, filename=name)
     token = processing.start()
     XRayStorage['UniqueSessionId'] = token
+    result = []
 
-    return {"UniqueSEssionId ": token, "Name": InputImage.filename}
+    for dir_path, _, file_names in os.walk(f"./sessions/{XRayStorage['UniqueSessionId']}"):
+        for file_name in file_names:
+            if file_name.endswith(".jpg"):
+                file_dict = {
+                    "FileName": file_name,
+                    "Type": "Image"
+                }
+            if file_name.endswith(".txt"):
+                file_dict = {
+                    "FileName": file_name,
+                    "Type": "Metadata"
+                }
+
+            result.append(file_dict)
+    XRayStorage["Files"] = result
+    return XRayStorage
 
 
 @app.get("/sessions")
 def sessions():
+    """
+    List all available sessions along with their associated files.
+
+    :return: List of sessions, each containing session information and associated files.
+    """
     result = []
 
     for dir_path, _, file_names in os.walk("./sessions"):
@@ -57,10 +85,17 @@ def sessions():
             for file_name in file_names:
                 if file_name.endswith(".jpg"):
                     file_dict = {
-                        "FileName": file_name[:-4],
-                        "Type": "jpg"
+                        "FileName": file_name,
+                        "Type": "Image"
                     }
-                    session_dict["Files"].append(file_dict)
+
+                if file_name.endswith(".txt"):
+                    file_dict = {
+                        "FileName": file_name,
+                        "Type": "Metadata"
+                    }
+
+                session_dict["Files"].append(file_dict)
 
             result.append(session_dict)
 
@@ -68,45 +103,71 @@ def sessions():
 
 
 @app.get("/session/{uniqueSessionId}")
-def unique_session(uniqueSessionId: str):
+def unique_session(unique_session_id: str):
+    """
+    List files associated with a current session.
+
+    :return: List of files within the current session.
+    """
     result = []
 
-    for dir_path, _, file_names in os.walk(f"./sessions/{str(uniqueSessionId)}"):
+    for dir_path, _, file_names in os.walk(f"./sessions/{unique_session_id}"):
         for file_name in file_names:
             if file_name.endswith(".jpg"):
                 file_dict = {
-                    "FileName": file_name[:-4],
-                    "Type": "jpg"
+                    "FileName": file_name,
+                    "Type": "Image"
                 }
+
+            if file_name.endswith(".txt"):
+                file_dict = {
+                    "FileName": file_name,
+                    "Type": "Metadata"
+                }
+
             result.append(file_dict)
 
     return result
 
 
 @app.get("/session/{uniqueSessionId}/image/{fileName}")
-def unique_session_image(uniqueSessionId: str, fileName: str):
-    folder = uniqueSessionId
-    file_name = fileName
+def unique_session_image(unique_session_id: str, file_name: str):
+    """
+    Stream an X-ray image file from a specific session.
 
+    :return:
+    """
     def file_iterator():
-        with open(
-            os.path.join(f"./sessions/{folder}", f"{file_name}.jpg"), mode="rb"
-        ) as file:
-            while True:
-                chunk = file.read(65536)
-                if not chunk:
-                    break
-                yield chunk
+        for _, _, file_names in os.walk(f"./sessions/{unique_session_id}"):
+            for filename in file_names:
+                with open(
+                    os.path.join(f"./sessions/{unique_session_id}/", f"{file_name}.jpg"), mode="rb"
+                ) as file:
+                    while True:
+                        chunk = file.read(65536)
+                        if not chunk:
+                            break
+                        yield chunk
 
     return StreamingResponse(file_iterator(), media_type="application/octet-stream")
 
 
 @app.get("/session/{uniqueSessionId}/meta/{fileName}")
-def unique_session_meta():
+def unique_session_meta(unique_session_id: str, file_name: str):
     pass
 
 
 @app.delete("/session/{uniqueSessionId}")
-def delete_session(uniqueSessionId: str):
-    print("paths " + os.path.join(os.getcwd(), f"sessions/{uniqueSessionId}"))
-    shutil.rmtree(f"./sessions/{uniqueSessionId}")
+def delete_session(unique_session_id: str):
+    """
+    Delete current session and all associated files.
+
+    :return: None
+    """
+    session_dir = os.path.join("./sessions/", f"{unique_session_id}")
+
+    if os.path.exists(session_dir):
+        shutil.rmtree(session_dir)
+        XRayStorage['UniqueSessionId'] = None
+    else:
+        raise HTTPException(status_code=404, detail="Session not found")
